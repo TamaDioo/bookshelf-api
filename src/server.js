cef70a4d3c347ad37ae659af0076e8963fa25771
Dio Andika Pradana Mulia Tama <tamadio91@gmail.com>
2026-04-01 16:27:54 +0700

    Update port and add root route for PaaS deployment

--- src/server.js
+++ src/server.js
@@ -3,13 +3,21 @@ import cors from 'cors';
 import routes from './routes.js';
 
 const app = express();
-const port = 9000;
-const host = 'localhost';
+const port = process.env.PORT || 9000;
+const host = '0.0.0.0';
 
 app.use(cors({ origin: '*' }));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the Bookshelf API',
+    appName: process.env.APP_NAME || 'Nama App belum diset',
+  });
+});
+
 app.use('/', routes);
 
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}`);
 });
